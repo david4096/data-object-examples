ed3b7e8a82629092881c757b594b35d36dbf186f
David Steinberg <david@resium.com>
2018-05-10 16:54:30 -0700

    Add script to rebuild json Delete old json

--- make_files.py
+++ make_files.py
@@ -7,16 +7,14 @@ client = Client("http://ec2-52-26-45-130.us-west-2.compute.amazonaws.com:8080/ga
 lc = client.client
 models = client.models
 
-response = {}
-response['next_page_token'] = "0"
+ListDataObjectsRequest = models.get_model('ListDataObjectsRequest')
+
+response = models.get_model('ListDataObjectsResponse')
+response.next_page_token = "0"
 
 def write_object(data_object):
-    with open(data_object['id'], 'w') as outfile:
+    with open('ga4gh/dos/v1/dataobjects/' + data_object['id'], 'w') as outfile:
         json.dump(data_object.marshal(), outfile)
 
-#while (response.get('next_page_token', None)):
-ListDataObjectsRequest = models.get_model('ListDataObjectsRequest')
-list_request = ListDataObjectsRequest(page_token="0")
-response = lc.ListDataObjects(body=list_request).result()
-map(write_object, response.data_objects)
-    #break
+list_response = ListDataObjectsRequest(page_token=response.next_page_token).result()
+map(write_object, list_response.data_objects)
